8f2504d2eafa46707e077b11c5320fe03e663998
Abdul Wahab Qarizada <ab_wahabqarizada@outlook.com>
2022-06-04 16:39:11 +0430

    validation

--- resources/views/QamarCardCard/Create.blade.php
+++ resources/views/QamarCardCard/Create.blade.php
@@ -364,7 +364,7 @@
                                 <div class="mb-3 position-relative">
                                     <label for="LevelPoverty" class="form-label">Level Of Poverty</label>
                                     <select class="form-select form-select-lg" required name="LevelPoverty" id="LevelPoverty">
-                                   <option value="1">1</option>
+                                   <option value="Poorest">1</option>
                            </select>
                                 </div>
                             </div>
