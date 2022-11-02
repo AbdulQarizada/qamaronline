ed3b12da20ea3f4857db25b879c3b02fbc6530a9
Abdul Qarizada <ab_wahabqarizada@outlook.com>
2022-11-02 23:56:22 +0430

    charts addedsdfdsfcvcvsdfsdfdsf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -903,15 +903,7 @@
 <script src="{{ URL::asset('/assets/js/pages/form-advanced.init.js') }}"></script>
 <script>
     var GenderChart = {
-        series: [{
-            {
-                $qamarcarecardsMale
-            }
-        }, {
-            {
-                $qamarcarecardsFemale
-            }
-        }],
+        series: [{{$qamarcarecardsMale}}, {{$qamarcarecardsFemale}}],
         chart: {
             width: 380,
             type: 'pie',
@@ -954,40 +946,8 @@
                 fontWeight: "500"
             }
         },
-        series: [{
-            {
-                $qamarcarecardsPoor
-            }
-        }, {
-            {
-                $qamarcarecardsLowIncome
-            }
-        }, {
-            {
-                $qamarcarecardsWidow
-            }
-        }, {
-            {
-                $qamarcarecardsOrphans
-            }
-        }, {
-            {
-                $qamarcarecardsDisabledIndividual
-            }
-        }, {
-            {
-                $qamarcarecardsElderlyIndividual
-            }
-        }, {
-            {
-                $qamarcarecardsDisplacedFamily
-            }
-        }, {
-            {
-                $qamarcarecardsDisasterAffected
-            }
-        }],
-        labels: ['Poor', 'Low Income', 'Widow', 'Orphans', 'Disabled Individual', 'Elderly Individual', 'Displaced Family', 'Disaster Affected'],
+        series: [{{$qamarcarecardsPoor}}, {{$qamarcarecardsLowIncome}},{{$qamarcarecardsWidow}}, {{$qamarcarecardsOrphans}},{{$qamarcarecardsDisabledIndividual}}, {{$qamarcarecardsElderlyIndividual}},{{$qamarcarecardsDisplacedFamily}}, {{$qamarcarecardsDisasterAffected}}],
+        labels: ['Poor', 'Low Income',  'Widow', 'Orphans', 'Disabled Individual', 'Elderly Individual', 'Displaced Family', 'Disaster Affected'],
         colors: ["#34c38f", "#556ee6", "#f46a6a", "#50a5f1", "#f1b44c", "#f1b44c", "#f1b44c", "#f1b44c", "#f1b44c"],
         legend: {
             show: !0,
@@ -1017,107 +977,109 @@
 
     var DataInsertionChart = {
 
-        chart: {
-            height: 350,
-            type: "bar",
-            toolbar: {
-                show: !1
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    position: "top"
-                }
-            }
-        },
+chart: {
+    height: 350,
+    type: "bar",
+    toolbar: {
+        show: !1
+    }
+},
+plotOptions: {
+    bar: {
         dataLabels: {
-            enabled: !0,
-            formatter: function(e) {
-                return e + "%"
-            },
-            offsetY: -22,
-            style: {
-                fontSize: "12px",
-                colors: ["#304758"]
-            }
-        },
-        series: [{
-            name: "Card Inserted",
-            data: [2.5, 3.2, 5, 10.1, 4.2, 3.8, 3, 2.4, 4, 1.2, 3.5, .8]
-        }],
-        colors: ["#556ee6"],
-        grid: {
-            borderColor: "#f1f1f1"
-        },
-        xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            position: "top",
-            labels: {
-                offsetY: -18
-            },
-            axisBorder: {
-                show: !1
-            },
-            axisTicks: {
-                show: !1
-            },
-            crosshairs: {
-                fill: {
-                    type: "gradient",
-                    gradient: {
-                        colorFrom: "#D8E3F0",
-                        colorTo: "#BED1E6",
-                        stops: [0, 100],
-                        opacityFrom: .4,
-                        opacityTo: .5
-                    }
-                }
-            },
-            tooltip: {
-                enabled: !0,
-                offsetY: -35
-            }
-        },
+            position: "top"
+        }
+    }
+},
+dataLabels: {
+    enabled: !0,
+    formatter: function(e) {
+        return e + "%"
+    },
+    offsetY: -22,
+    style: {
+        fontSize: "12px",
+        colors: ["#304758"]
+    }
+},
+series: [{
+    name: "Card Inserted",
+    data: [2.5, 3.2, 5, 10.1, 4.2, 3.8, 3, 2.4, 4, 1.2, 3.5, .8]
+}],
+colors: ["#556ee6"],
+grid: {
+    borderColor: "#f1f1f1"
+},
+xaxis: {
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    position: "top",
+    labels: {
+        offsetY: -18
+    },
+    axisBorder: {
+        show: !1
+    },
+    axisTicks: {
+        show: !1
+    },
+    crosshairs: {
         fill: {
+            type: "gradient",
             gradient: {
-                shade: "light",
-                type: "horizontal",
-                shadeIntensity: .25,
-                gradientToColors: void 0,
-                inverseColors: !0,
-                opacityFrom: 1,
-                opacityTo: 1,
-                stops: [50, 0, 100, 100]
-            }
-        },
-        yaxis: {
-            axisBorder: {
-                show: !1
-            },
-            axisTicks: {
-                show: !1
-            },
-            labels: {
-                show: !1,
-                formatter: function(e) {
-                    return e + "%"
-                }
-            }
-        },
-        title: {
-            text: "Montly Data Insertion, 2002",
-            floating: !0,
-            offsetY: 330,
-            align: "center",
-            style: {
-                color: "#444",
-                fontWeight: "500"
+                colorFrom: "#D8E3F0",
+                colorTo: "#BED1E6",
+                stops: [0, 100],
+                opacityFrom: .4,
+                opacityTo: .5
             }
         }
+    },
+    tooltip: {
+        enabled: !0,
+        offsetY: -35
+    }
+},
+fill: {
+    gradient: {
+        shade: "light",
+        type: "horizontal",
+        shadeIntensity: .25,
+        gradientToColors: void 0,
+        inverseColors: !0,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [50, 0, 100, 100]
+    }
+},
+yaxis: {
+    axisBorder: {
+        show: !1
+    },
+    axisTicks: {
+        show: !1
+    },
+    labels: {
+        show: !1,
+        formatter: function(e) {
+            return e + "%"
+        }
+    }
+},
+title: {
+    text: "Montly Data Insertion, 2002",
+    floating: !0,
+    offsetY: 330,
+    align: "center",
+    style: {
+        color: "#444",
+        fontWeight: "500"
+    }
+}
     };
 
     var DataInsertionChart = new ApexCharts(document.querySelector("#DataInsertionChart"), DataInsertionChart);
     DataInsertionChart.render();
+
+
 </script>
 @endsection
