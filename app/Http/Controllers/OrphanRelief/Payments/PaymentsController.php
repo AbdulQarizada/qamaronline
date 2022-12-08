25b586d697c95b18acbe3d9e1a02b1a34c307990
Abdul Qarizada <ab_wahabqarizada@outlook.com>
2022-12-09 00:19:25 +0430

    User Management 5

--- app/Http/Controllers/OrphanRelief/Payments/PaymentsController.php
+++ app/Http/Controllers/OrphanRelief/Payments/PaymentsController.php
@@ -177,7 +177,7 @@ class PaymentsController extends Controller
           'FullName' => request('FullName'),
           'email' => request('Email'),
           'password' => Hash::make($RandomPassword),
-          'Profile' => 'avater.jpg',
+          'Profile' => 'avatar.jpg',
           'IsActive' => 1,
           'IsOrphanSponsor' => 1,
         ]);
